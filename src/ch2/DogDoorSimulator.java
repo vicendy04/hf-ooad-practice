25680bf038454ecd6016d1588d5a0ea89b09f1f9
vicendy04 <htvd1204@gmail.com>
2024-02-26 20:44:49 +0700

    Add initial files to package ch2

---
+++ src/ch2/DogDoorSimulator.java
@@ -0,0 +1,16 @@
+package ch2;
+
+public class DogDoorSimulator {
+    public static void main(String[] args) {
+        DogDoor door = new DogDoor();
+        Remote remote = new Remote(door);
+        System.out.println("Fido barks to go outside...");
+        remote.pressButton();
+        System.out.println("\nFido has gone outside...");
+        remote.pressButton();
+        System.out.println("\nFido’s all done...");
+        remote.pressButton();
+        System.out.println("\nFido’s back inside...");
+        remote.pressButton();
+    }
+}
