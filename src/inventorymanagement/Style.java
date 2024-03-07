055550629361dffc9579bf841cd50981b08b9002
vicendy04 <htvd1204@gmail.com>
2024-03-07 23:17:24 +0700

    feat: create abstract base classes for upcoming feature

---
+++ src/inventorymanagement/Style.java
@@ -0,0 +1,17 @@
+package inventorymanagement;
+
+public enum Style {
+
+    A, F;
+
+    public String toString() {
+        switch (this) {
+            case A:
+                return "A style";
+            case F:
+                return "F style";
+            default:
+                return "Unspecified";
+        }
+    }
+}
